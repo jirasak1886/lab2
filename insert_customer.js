cc15c7dd1a7f9cf507de79b72f08eaab9e112db2
jirasak <jirasakwaennak1776@gmail.com>
2024-07-03 12:01:22 +0700

    new lab2

---
+++ insert_customer.js
@@ -0,0 +1,26 @@
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database: "mydb"
+});
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+
+    var sql = "INSERT INTO Customers (name, address) VALUES ?";
+    var values = [
+        ['tai', 'vscode'],
+        ['boy', 'vscode'],
+        ['aik', 'vscode'],
+        ['Pleng', 'vscode'],
+        ['Gink', 'vscode']
+    ];
+
+    con.query(sql, [values], function(err, result) {
+        if (err) throw err;
+        console.log("Customer Created.");
+    });
+
+});
---
+++ select_customer.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database: "mydb"
+});
+con.connect(function(err){
+    if(err)throw err;
+    console.log("connect");
+    var sql="SELECT*FROM Customers";
+    con.query(sql,function(err,result,fields){
+        if (err)throw err;
+        console.log(result);
+        result.forEach((row)=>{
+            console.log(`ID:{row['id']},NAME:${row['name']},ADDRESS:${row['address']}`);
+        });
+    });
+});
+
